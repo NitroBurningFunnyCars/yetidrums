37f8c5425eb4d24c16ea09f321beedadf8455cee
Christopher Mahmood <chris.mahmood+github@gmail.com>
2024-10-01 16:17:50 -0700

    New theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yetidrums.com',
-})
+	integrations: [tailwind()],
+});
